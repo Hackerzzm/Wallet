52f8ae5ba8cdcd5835ed2a2e95d7343e29e8e837
Hacker_zzm <zzmhacker@gmail.com>
2022-02-08 16:24:29 +0800

    沉浸式状态栏 https://blog.csdn.net/qq_34681580/article/details/103955191 6.0及以上白底黑字 5.0及以上透明底白字 5.0以下不4.4及以上显示隐藏 4.4以下不支持

--- app/src/main/java/com/zzm/wallet/core/BaseActivity.kt
+++ app/src/main/java/com/zzm/wallet/core/BaseActivity.kt
@@ -1,8 +1,13 @@
 package com.zzm.wallet.core
 
 import android.content.Context
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.zzm.wallet.R
 import com.zzm.wallet.db.AppDatabase
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -13,10 +18,22 @@ abstract class BaseActivity : AppCompatActivity() {
     mContext = this
     dbInstance = AppDatabase.getInstance(mContext)!!
     MyApplication.addActivity(this)
+    statusBarSet()
   }
 
   override fun onDestroy() {
     super.onDestroy()
     MyApplication.finishActivity(this)
+  }
+
+  private fun statusBarSet(){
+    if (VERSION.SDK_INT >= VERSION_CODES.M) {
+      window.decorView.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }else {
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+      window.statusBarColor = resources.getColor(R.color.colorPrimary)
+    }
   }
 }
